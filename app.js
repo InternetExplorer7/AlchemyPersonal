bf9754ce810c4983fb5621ffe2febebf09e17fdf
InternetExplorer7 <themaster231436@gmail.com>
2015-07-15 19:57:20 -0700

    Large update, second push.

--- app.js
+++ app.js
@@ -14,7 +14,6 @@
    limitations under the License.
 */
 
-
 var express = require('express');
 var consolidate = require('consolidate');
 
@@ -34,77 +33,96 @@ var MongoClient = require('mongodb').MongoClient; // MONGO DB
 var io = require('socket.io')(server);
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+        app.use(express.errorHandler());
 }
 
 
 
 
 var port = process.env.PORT || 3000;
-server.listen(port, function(){
-	console.log('Express server listening on port ' + port);
+server.listen(port, function() {
+        console.log('Express server listening on port ' + port);
 });
 
-MongoClient.connect('MongoDB connection string', function(err, db){
+MongoClient.connect('MongoDB user string', function(err, db) {
 
-	console.log("Got to callback");
-	io.on('connection', function(socket){
-		socket.on('register', function(username, password, first, last, email, callback){
-			var auth = Math.round(new Date().getTime()/1000.0)
-			db.collection('users').insert({_id: auth, username: username, password: password, first: first, last: last, email: email, interests: []});
-			db.collection('users').findOne({username: username}, function(err, doc){
-				if(doc === null){
-					callback(auth, true);
-				}
-				else{
-					callback(auth, false);
-				}
-			});
-		}); // register
+        io.on('connection', function(socket) {
+                console.log("New user connected");
+                socket.on('register', function(username, password, first, last, email, callback) {
+                        var auth = Math.round(new Date().getTime() / 1000.0)
+                        db.collection('users').findOne({
+                                username: username
+                        }, function(err, doc) {
+                                if (doc === null) {
+                                        db.collection('users').insert({
+                                                _id: auth,
+                                                username: username,
+                                                password: password,
+                                                first: first,
+                                                last: last,
+                                                email: email,
+                                                interests: []
+                                        });
+                                        callback(auth, true);
+                                } else {
+                                        callback(auth, false);
+                                }
+                        });
+                }); // register
 
-		socket.on('login', function(username, password, callback){
-					db.collection('users').findOne({username: username}, function(err, doc){
-						if(doc === null){
-							console.log('false');
-							callback(false, doc._id);
+                socket.on('login', function(username, password, callback) {
+                        db.collection('users').findOne({
+                                username: username
+                        }, function(err, doc) {
+                                if (doc === null) {
+                                        console.log('false');
+                                        callback(false);
+                                } else if (doc.password === password) {
+                                        console.log('true');
+                                        callback(true, doc._id);
+                                } else {
+                                        console.log('password mismatch');
+                                        callback(false, doc._id);
+                                }
+                        });
+                });
+                socket.on('process', function(text, id) {
+                        console.log("ID inside auth " + id);
+                        alchemyapi.keywords('text', text, {
+                                'sentiment': 1
+                        }, function(response) {
+                                console.log(response);
+                                response.keywords.forEach(function(item) {
+                                        console.log(item.text);
+                                        db.collection('users').update({
+                                                _id: parseInt(id)
+                                        }, {
+                                                $push: {
+                                                        interests: item.text
+                                                }
+                                        });
+                                });
+                                //find(id);
+                        }); // AlchemyAPI callback
+                }); // process
 
-						}
-						else if(doc.password === password){
-							console.log('true');
-							callback(true, doc._id);
-						}
-						else{
-							console.log('password mismatch');
-							callback(false, doc._id);
-						}
-					});
-		});
-		socket.on('process', function(text, auth){
-	alchemyapi.entities('text', text,{ 'sentiment':1 }, function(response) {
-		console.log(response);
-		response.entities.forEach(function(item){
-					console.log(item.text);
-					db.collection('users').update({_id: auth}, { $push: { interests: item.text }});
-				});
-			find(auth);
-			}); // AlchemyAPI callback
-		}); // process
+                socket.on('get', function(id) {
+                        console.log('got to get');
+                        find(id);
+                }); // get
 
-		socket.on('get', function(id){
-			console.log('got to get');
-			find(id);
-		}); // get
-
-			function find(id){
-				setTimeout(function(){
-					db.collection('users').findOne({_id: id}, function(err, doc){
-					console.log(doc);	
-					io.emit('start', doc);
-					});
-				}, 1000);
-		}
-	}); // SocketIO connection
+                function find(id) {
+                        setTimeout(function() {
+                                console.log("ID inside find " + id);
+                                db.collection('users').findOne({
+                                        _id: parseInt(id)
+                                }, function(err, doc) {
+                                        console.log(doc);
+                                        io.emit('start', doc);
+                                });
+                        }, 1000);
+                }
+        }); // SocketIO connection
 
 
 }); // MongoDB
-
--- public/main.js
+++ public/main.js
@@ -1,29 +1,12 @@
 
-$("#form").hide();
-$("#tell").hide();
-$("#main").hide();
-$("#pageone").hide();
-$("#loginform").hide();
-$("nav").hide();
-$(window).load(function(){
-	$("#loading").fadeOut(500);
-	$("nav").fadeIn(500);
-	$("#pageone").fadeIn(1000);
+$(document).ready(function(){
 	var socket = io();
-	var identification;
-
-	$("#signin").click(function(){
-		$("#pageone").fadeOut(1000);
-		$("#loginform").fadeIn(2000);
-	});
 
 	$("#loginuser").submit(function(){
 		socket.emit("login", $("#usern").val(), $("#passw").val(), function(check, id){
 			if(check === true){
-			identification = id;
-			socket.emit('get', identification);
-			$("#loginform").fadeOut(1000);
-			$("#main").fadeIn(4000);
+			localStorage.setItem('uid', id);
+			window.location.replace('profile.html');
 			}
 			else{
 				alert("Wrong Username/Password");
@@ -32,47 +15,41 @@ $(window).load(function(){
 		return false;
 	});
 
-
-	$("#register").click(function(){ // User is signing up for a new account
-		$("#pageone").fadeOut(1000);
-		$("#form").fadeIn(2000);
-	});
-
 	$("#target").submit(function(){
 		socket.emit('register', $("#username").val(), $("#password").val(), $("#first").val(), $("#last").val(), $("#email").val(), function(id, verify){
+			console.log("Verify ==== " + verify);
 			if(verify === true){
-			identification = id;
-			$("#form").fadeOut(1000);
-			$("#tell").fadeIn(2000);
+			localStorage.setItem('uid', id);
+			console.log("ID in first: " + localStorage.getItem('uid'));
+		    window.location.replace("write.html");
+
 			}
 			else{
 				alert("That username is already taken");
 			}
 		});
 		return false;
-
 	});
 
 	$("#interests").submit(function(){ // User submitted personal info
-		socket.emit('process', $("#info").val(), identification);
-		$("#tell").fadeOut(1000);
-		$("#main").fadeIn(4000);
+		console.log("Sending UID: " + localStorage.getItem('uid'));
+		socket.emit('process', $("#info").val(), localStorage.getItem('uid'));
+		window.location.replace("/profile.html");
 		return false;
 	});
 
 
-
 	socket.on('start', function(data){
-		if(data._id === identification){ // Authentication
+		if(data._id === parseInt(localStorage.getItem('uid'))){ // Authentication
 			console.log('got to start data: ' + data);
-		data.interests.forEach(function(item){
+			data.interests.forEach(function(item){
 				console.log(item);
-				$.getJSON('https://www.googleapis.com/customsearch/v1?key=AIzaSyBXtEoqtV9NPkbmxgFXpeKnokzyPMRKxIk&cx=004477128878591810569:azzlpvtod_s&q=' + item, function(data){
-					console.log(data);
-					//console.log("Image location: " + data.items[1].pagemap.article[0].articlebody);
-					console.log(data.items[0].pagemap.cse_thumbnail[0].src.length);
-					$("#main").append("<div class='box'> <a href=" + data.items[0].link + " target='_blank'> <img src=" + data.items[0].pagemap.cse_thumbnail[0].src + " /> <div id='text'>  <div id='title'>   <h2>" + data.items[0].title + "</h2> </div> <div class='snippet'> <p>" + data.items[0].snippet +  "</p> </div> </div> </a> </div>");
-				});
+				$.getJSON('https://www.googleapis.com/customsearch/v1?key=AIzaSyBXt9o4tV9NP3bmxgFXpzKnokzyPMRKxIk&cx=004477161878521810569:azzlpvtod_s&q=' + item + " news", function(data){
+					console.log(data.items[0].link);
+					console.log(data.items[0].title); 
+					console.log(data.items[0].snippet);
+					$("#overview").prepend("<section class='section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp'>             <div class='mdl-card mdl-cell mdl-cell--12-col'> <div class='mdl-card__supporting-text'> <h4>" + data.items[0].title + "</h4>" + data.items[0].snippet + "</div> <div class='mdl-card__actions'> <a href=' " + data.items[0].link + " ' class='mdl-button' target='_blank'>Read full article</a> </div> </div> </section>"); // Left off here 
+				}); 
 			});
 		}
 	});
